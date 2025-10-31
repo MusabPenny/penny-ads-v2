2be732d07f797b5056266a045687440db7a0d5e1
MusabKozlic <kozlic8@gmail.com>
2025-10-31 13:30:56 +0100

    socket server url mismatch

--- Ad-displays-admin-dashboard/socket.ts
+++ Ad-displays-admin-dashboard/socket.ts
@@ -2,7 +2,7 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_BASE_URL);
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL);
 socket.on("connect", () => {
   console.log("Connected to socket server");
 });
